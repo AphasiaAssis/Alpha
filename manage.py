3587d9ba1a2c8b0d234afd714796c9a96e3678d1
zhengjian2322 <2322171400@qq.com>
2018-05-04 00:28:37 +0800

    Signed-off-by: zhengjian2322 <2322171400@qq.com>

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "APH.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "upload_and_show.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
